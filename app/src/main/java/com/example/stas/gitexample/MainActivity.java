67e1949623b7718bc8bdc72e42a481289600b364
Stas Stahma <StasStahma@gmail.com>
2018-07-11 23:39:18 +0300

    Some changes added

--- app/src/main/java/com/example/stas/gitexample/MainActivity.java
+++ app/src/main/java/com/example/stas/gitexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stas.gitexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Changed", Toast.LENGTH_SHORT).show();
     }
 }
